1493aeeb9dbe6038e1598f2f15bb362171450ba7
cjohn033 <cjohn033@ilscha03-cdk-40.uscc.com>
2019-02-20 12:29:26 -0600

    Add train class for the ANN to be trained

---
+++ train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 20 12:19:15 2019
+
+@author: Matt Harvey (https://blog.coast.ai/@harvitronix), 
+         reimplemented by nellac77
+
+train.py: Utility used by the Network class to actually train.
+
+Based on:
+    https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+"""
+from keras.datasets import mnist, cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
+
+# Helper: Early stopping.
+early_stopper = EarlyStopping(patience=5)
+
+def get_cifar10():
+    """Retrieve the CIFAR dataset and process the data."""
+    # Set defaults.
+    nb_classes = 10
+    batch_size = 64
+    input_shape = (3072,)
+
+    # Get the data.
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = x_train.reshape(50000, 3072)
+    x_test = x_test.reshape(10000, 3072)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+
+    # convert class vectors to binary class matrices
+    y_train = to_categorical(y_train, nb_classes)
+    y_test = to_categorical(y_test, nb_classes)
+
